8e8eecb1ea09042a963d94f76861b2f45b9cd48d
IuliaNeagoe <ayluyneag@yahoo.com>
2014-01-26 16:58:09 +0200

    proiect visual

---
+++ Aplicatie_medicala/Presentation Layer/Aplicatie.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Aplicatie_medicala
+{
+    partial class Aplicatie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbPacienti = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgPacienti = new System.Windows.Forms.DataGridView();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tbCauta = new System.Windows.Forms.TabPage();
+            this.tbTratament = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tbPacienti.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPacienti)).BeginInit();
+            this.tabControl2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.exit;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(910, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 29);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tbPacienti);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 47);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(928, 426);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tbPacienti
+            // 
+            this.tbPacienti.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.backgound;
+            this.tbPacienti.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tbPacienti.Controls.Add(this.tabControl2);
+            this.tbPacienti.Controls.Add(this.dgPacienti);
+            this.tbPacienti.Location = new System.Drawing.Point(4, 22);
+            this.tbPacienti.Name = "tbPacienti";
+            this.tbPacienti.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPacienti.Size = new System.Drawing.Size(920, 400);
+            this.tbPacienti.TabIndex = 0;
+            this.tbPacienti.Text = "Pacienti";
+            this.tbPacienti.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.backgound;
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(920, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Personal";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgPacienti
+            // 
+            this.dgPacienti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPacienti.Location = new System.Drawing.Point(25, 15);
+            this.dgPacienti.Name = "dgPacienti";
+            this.dgPacienti.Size = new System.Drawing.Size(879, 195);
+            this.dgPacienti.TabIndex = 0;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tbCauta);
+            this.tabControl2.Controls.Add(this.tbTratament);
+            this.tabControl2.Location = new System.Drawing.Point(25, 227);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(879, 167);
+            this.tabControl2.TabIndex = 1;
+            // 
+            // tbCauta
+            // 
+            this.tbCauta.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.backgound;
+            this.tbCauta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tbCauta.Location = new System.Drawing.Point(4, 22);
+            this.tbCauta.Name = "tbCauta";
+            this.tbCauta.Padding = new System.Windows.Forms.Padding(3);
+            this.tbCauta.Size = new System.Drawing.Size(871, 141);
+            this.tbCauta.TabIndex = 0;
+            this.tbCauta.Text = "Cauta";
+            this.tbCauta.UseVisualStyleBackColor = true;
+            // 
+            // tbTratament
+            // 
+            this.tbTratament.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.backgound;
+            this.tbTratament.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tbTratament.Location = new System.Drawing.Point(4, 22);
+            this.tbTratament.Name = "tbTratament";
+            this.tbTratament.Padding = new System.Windows.Forms.Padding(3);
+            this.tbTratament.Size = new System.Drawing.Size(871, 141);
+            this.tbTratament.TabIndex = 1;
+            this.tbTratament.Text = "Tratament";
+            this.tbTratament.UseVisualStyleBackColor = true;
+            // 
+            // Aplicatie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.backgound;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(952, 490);
+            this.ControlBox = false;
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Aplicatie";
+            this.Text = "Aplicatie";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tbPacienti.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPacienti)).EndInit();
+            this.tabControl2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbPacienti;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tbCauta;
+        private System.Windows.Forms.TabPage tbTratament;
+        private System.Windows.Forms.DataGridView dgPacienti;
+    }
+}
---
+++ Aplicatie_medicala/Presentation Layer/Login.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Aplicatie_medicala
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbUsername = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbUsername
+            // 
+            this.txbUsername.Location = new System.Drawing.Point(90, 100);
+            this.txbUsername.Name = "txbUsername";
+            this.txbUsername.Size = new System.Drawing.Size(100, 20);
+            this.txbUsername.TabIndex = 0;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(90, 142);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(100, 20);
+            this.txbPassword.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblUsername.ForeColor = System.Drawing.Color.Black;
+            this.lblUsername.Location = new System.Drawing.Point(12, 101);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(73, 15);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            this.lblUsername.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPass.ForeColor = System.Drawing.Color.Black;
+            this.lblPass.Location = new System.Drawing.Point(11, 147);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(69, 15);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Password";
+            this.lblPass.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.descărcare;
+            this.btnLogin.Location = new System.Drawing.Point(30, 189);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.descărcare;
+            this.btnExit.Location = new System.Drawing.Point(152, 189);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Aplicatie_medicala.Properties.Resources.descărcare;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(477, 245);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.txbUsername);
+            this.MaximumSize = new System.Drawing.Size(493, 283);
+            this.MinimumSize = new System.Drawing.Size(493, 283);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbUsername;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
